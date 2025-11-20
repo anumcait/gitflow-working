bca262fd9afee224b13dbd595928b5f21cb651c0
anumcait <53853412+anumcait@users.noreply.github.com>
2025-11-20 15:42:56 +0530

    Add functions for addition and subtraction

---
+++ app/service.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    if a is None or b is None:
+        raise ValueError("Inputs cannot be None")
+    return a + b
+
+def subtract_numbers(a,b):
+    if a is None or b is None:
+        raise ValueError("Inputs cannot be None")
+    return a-b
