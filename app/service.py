4454528678c574b4375b85d5888c38aa508b1657
anumcait <53853412+anumcait@users.noreply.github.com>
2025-11-21 11:37:18 +0530

    Update service.py

--- app/service.py
+++ app/service.py
@@ -1,6 +1,6 @@
 def add_numbers(a, b):
     if a is None or b is None:
-        raise ValueError("Inputs cannot be None")
+        raise ValueError("Inputs cannot be None1")
     return a + b
 
 def subtract_numbers(a,b):
